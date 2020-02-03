45d1cd960a3248c5fb9fbeb80609c49953ece440
FiglerReNi <renatafigler85@gmail.com>
2020-02-03 20:40:59 +0100

    forras

--- script/ui.js
+++ script/ui.js
@@ -43,9 +43,54 @@ $(document).ready(function () {
             $('#value3').val( ui.value);
         }
     });
+    $('#myslider4').slider({
+        value: 0, //kezdő érték
+        min: -90, //min érték
+        max: 90, //max érték
+        step: 0.00001, //lépésköz
+        orientation: 'horizontal', //csúszka vízszintes vagy függőleges
+        slide: function( event, ui ) {
+            $('#value4').val( ui.value);
+        }
+    });
+    $('#myslider5').slider({
+        value: 0, //kezdő érték
+        min: -180, //min érték
+        max: 180, //max érték
+        step: 0.00001, //lépésköz
+        orientation: 'horizontal', //csúszka vízszintes vagy függőleges
+        slide: function( event, ui ) {
+            $('#value5').val( ui.value);
+        }
+    });
+    $('#red, #green, #blue').slider({
+        value: 127, //kezdő érték
+        range: 'min', //szines csík mozgása milyen legyen
+        max: 255, //max érték
+        orientation: 'horizontal', //csúszka vízszintes vagy függőleges
+        slide: refreshSwatch,
+        change: refreshSwatch
+    });
+
+    //A change-hez kell, hogy amikor betöltődik az oldal elinduljon a refreshSwatch function és színes legyen a kocka ne üres induláskor
+    $( "#red" ).slider( "value", 127 );
+    $( "#green" ).slider( "value", 127 );
+    $( "#blue" ).slider( "value", 127 );
+
+    $("button:submit").button();
+
+    function refreshSwatch() {
+        var red = $('#red').slider('value');
+        var green = $('#green').slider('value');
+        var blue =$('#blue').slider('value');
+        var color = 'rgb(' + red + ',' + green + ',' + blue + ')';
+        $('#red_val').val(red);
+        $('#green_val').val(green);
+        $('#blue_val').val(blue);
+        $('#color_val').val(color);
+        $('#swatch').css({"background-color" : color})
+    }
 });
-
-
 
 
 /* UI
